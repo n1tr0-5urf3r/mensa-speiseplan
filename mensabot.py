294707a9aa9019d488b748083af3cff2d8dd08e3
Fabian Ihle <fabi@ihlecloud.de>
2021-11-13 18:22:58 +0100

    Restrict to mensa channel

--- mensabot.py
+++ mensabot.py
@@ -56,79 +56,80 @@ slash = SlashCommand(bot, sync_commands=True)
         ])
 async def mensa(ctx: SlashContext, location=None, period=None):
 
-        def embed_list_lines(embed,
-                             lines,
-                             field_name,
-                             max_characters=1024,
-                             inline=False):
-            zero_width_space = u'\u200b'
-            value = "\n".join(lines)
-            if len(value) > 1024:
-                value = ""
-                values = []
-                for line in lines:
-                    if len(value) + len(line) > 1024:
-                        values.append(value)
-                        value = ""
-                    value += line + "\n"
-                if value:
+    def embed_list_lines(embed,
+                            lines,
+                            field_name,
+                            max_characters=1024,
+                            inline=False):
+        zero_width_space = u'\u200b'
+        value = "\n".join(lines)
+        if len(value) > 1024:
+            value = ""
+            values = []
+            for line in lines:
+                if len(value) + len(line) > 1024:
                     values.append(value)
-                embed.add_field(name=field_name,
-                                value=values[0], inline=inline)
-                for v in values[1:]:
-                    embed.add_field(name=zero_width_space,
-                                    value=v, inline=inline)
-            else:
-                embed.add_field(name=field_name, value=value, inline=inline)
-            return embed
+                    value = ""
+                value += line + "\n"
+            if value:
+                values.append(value)
+            embed.add_field(name=field_name,
+                            value=values[0], inline=inline)
+            for v in values[1:]:
+                embed.add_field(name=zero_width_space,
+                                value=v, inline=inline)
+        else:
+            embed.add_field(name=field_name, value=value, inline=inline)
+        return embed
 
-        def next_weekday(d, weekday):
-            days_ahead = weekday - d.weekday()
-            if days_ahead <= 0:  # Target day already happened this week
-                days_ahead += 7
-            return d + datetime.timedelta(days_ahead)
+    def next_weekday(d, weekday):
+        days_ahead = weekday - d.weekday()
+        if days_ahead <= 0:  # Target day already happened this week
+            days_ahead += 7
+        return d + datetime.timedelta(days_ahead)
 
-        def get_data(id):
-            # Get data
-            url_mensa = "https://www.my-stuwe.de/wp-json/mealplans/v1/canteens/{}?lang=de".format(
-                id)
-            r = requests.get(url_mensa)
-            r.encoding = 'utf-8-sig'
-            data = r.json()
-            return data
+    def get_data(id):
+        # Get data
+        url_mensa = "https://www.my-stuwe.de/wp-json/mealplans/v1/canteens/{}?lang=de".format(
+            id)
+        r = requests.get(url_mensa)
+        r.encoding = 'utf-8-sig'
+        data = r.json()
+        return data
 
-        def build_menu(data, caf=False):
-            menu = []
-            menu_cur_day = []
-            for id in data:
-                # If meal matches today
-                if str(day.date()) in id["menuDate"]:
-                    # Collect meal for this day
-                    if caf:
-                        menuLine = "Cafeteria"
-                    else:
-                        menuLine = id["menuLine"]
-                    if "Dessert" not in menuLine and "Beilagen" not in menuLine and "Salat" not in menuLine:
-                        price = id["studentPrice"]
-                        # Append newline to last entry
-                        if id["menu"]:
-                            id["menu"][-1] = id["menu"][-1] + "\n"
-                        for food in id["menu"]:
-                            if caf:
-                                if re.match("^Pommes frites$", food):
-                                    continue
-                            food = "-{}".format(food)
-                            menu.append(food)
-                        if not menu:
-                            continue
-                        # menu is fully available, build string
-                        menu_cur_day.append(
-                            ["*{} - {}€*".format(menuLine, price)])
-                        menu_cur_day.append(menu)
-                        # Reset menu
-                        menu = []
-            return menu_cur_day
+    def build_menu(data, caf=False):
+        menu = []
+        menu_cur_day = []
+        for id in data:
+            # If meal matches today
+            if str(day.date()) in id["menuDate"]:
+                # Collect meal for this day
+                if caf:
+                    menuLine = "Cafeteria"
+                else:
+                    menuLine = id["menuLine"]
+                if "Dessert" not in menuLine and "Beilagen" not in menuLine and "Salat" not in menuLine:
+                    price = id["studentPrice"]
+                    # Append newline to last entry
+                    if id["menu"]:
+                        id["menu"][-1] = id["menu"][-1] + "\n"
+                    for food in id["menu"]:
+                        if caf:
+                            if re.match("^Pommes frites$", food):
+                                continue
+                        food = "-{}".format(food)
+                        menu.append(food)
+                    if not menu:
+                        continue
+                    # menu is fully available, build string
+                    menu_cur_day.append(
+                        ["*{} - {}€*".format(menuLine, price)])
+                    menu_cur_day.append(menu)
+                    # Reset menu
+                    menu = []
+        return menu_cur_day
 
+    if ctx.channel.id == 123456:
         # Get time stuff
         today = datetime.datetime.now()
         cal_week = today.strftime("%W")
@@ -141,40 +142,40 @@ async def mensa(ctx: SlashContext, location=None, period=None):
         mensa_id = "621"  # Tuebingen Morgenstelle
         caf_id = "724"
         emoji_map = {"[S]": "[ :pig2: ]",
-                     "[R]": "[ :cow2: ]",
-                     "[S/R]": "[ :pig2: / :cow2: ]",
-                     "[F]": "[ :fish: ]",
-                     "[G]": "[ :rooster: ]",
-                     "[V]": "[ :seedling: ]",
-                     "[L]": "[ :sheep: ]",
-                     "[W]": "[ :deer: ]",
-                     "[vegan]": "[ <:vegan:643514903029743618> ]",
-                     "Tagesmenü -": ":spaghetti: Tagesmenü -",
-                     "Tagesmenü 2 -": ":spaghetti: Tagesmenü 2 -",
-                     "Tagesmenü vegetarisch -": ":seedling: Tagesmenü vegetarisch -",
-                     "mensaVital": ":apple: mensaVital",
-                     "Cafeteria": ":coffee: Cafeteria",
-                     "Angebot des Tages": ":dollar: Angebot des Tages"}
+                        "[R]": "[ :cow2: ]",
+                        "[S/R]": "[ :pig2: / :cow2: ]",
+                        "[F]": "[ :fish: ]",
+                        "[G]": "[ :rooster: ]",
+                        "[V]": "[ :seedling: ]",
+                        "[L]": "[ :sheep: ]",
+                        "[W]": "[ :deer: ]",
+                        "[vegan]": "[ <:vegan:643514903029743618> ]",
+                        "Tagesmenü -": ":spaghetti: Tagesmenü -",
+                        "Tagesmenü 2 -": ":spaghetti: Tagesmenü 2 -",
+                        "Tagesmenü vegetarisch -": ":seedling: Tagesmenü vegetarisch -",
+                        "mensaVital": ":apple: mensaVital",
+                        "Cafeteria": ":coffee: Cafeteria",
+                        "Angebot des Tages": ":dollar: Angebot des Tages"}
 
         if period:
-                if period == "Nächste Woche":
-                        cal_week = int(cal_week) + 1
-                        today = next_weekday(today, 0)
-                        weekday = 0
-                        week_start = today
-                        week_end = week_start + datetime.timedelta(days=4)
-                elif period == "Heute":
-                        heute_flag = True
+            if period == "Nächste Woche":
+                cal_week = int(cal_week) + 1
+                today = next_weekday(today, 0)
+                weekday = 0
+                week_start = today
+                week_end = week_start + datetime.timedelta(days=4)
+            elif period == "Heute":
+                heute_flag = True
 
         if location:
-                if location == "Shedhalle":
-                        # Mensa shedhalle
-                        mensa_id = "611"
-                elif location == "Nürtingen":
-                        # Nuertingen
-                        mensa_id = "665"
-                elif location == "Morgenstelle":
-                        mensa_id = "621"  # Tuebingen Morgenstelle
+            if location == "Shedhalle":
+                # Mensa shedhalle
+                mensa_id = "611"
+            elif location == "Nürtingen":
+                # Nuertingen
+                mensa_id = "665"
+            elif location == "Morgenstelle":
+                mensa_id = "621"  # Tuebingen Morgenstelle
 
         data = get_data(mensa_id)
         if location and location == "Nürtingen":
@@ -189,7 +190,7 @@ async def mensa(ctx: SlashContext, location=None, period=None):
 
         # Needed later
         wochentage = ["Montag", "Dienstag", "Mittwoch",
-                      "Donnerstag", "Freitag", "Samstag", "Sonntag"]
+                    "Donnerstag", "Freitag", "Samstag", "Sonntag"]
         needed_days = []
 
         # Show next week on weekends
@@ -219,7 +220,7 @@ async def mensa(ctx: SlashContext, location=None, period=None):
         else:
             embed = Embed(
                 description="{}, KW {} vom {} bis {}".format(canteen, cal_week, week_start.strftime("%d.%m."),
-                                                             week_end.strftime("%d.%m.")), color=color)
+                                                            week_end.strftime("%d.%m.")), color=color)
         for day in needed_days:
             cur_weekday = day.weekday()
             # Go through all meals (6/day)
@@ -251,6 +252,7 @@ async def mensa(ctx: SlashContext, location=None, period=None):
             url='https://upload.wikimedia.org/wikipedia/commons/thumb/b/bf/Studentenwerk_T%C3%BCbingen-Hohenheim_logo.svg/220px-Studentenwerk_T%C3%BCbingen-Hohenheim_logo.svg.png')
         embed.set_footer(text='Bot by Fabi / N1tR0#0914')
         await ctx.send(embed=embed)
-
+    else:
+        await ctx.send("Bitte nutze den dafür vorgesehenen Channel {.mention}".format(bot.get_channel(123456)))
 
 bot.run("ENTER TOKEN HERE")
